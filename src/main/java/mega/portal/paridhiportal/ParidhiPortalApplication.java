fb29cd30e8898849c184324e8aecd922889f5003
Atul Singh <cocatul11@gmail.com>
2024-03-07 01:12:20 +0530

    Add initial project files and dependencies

---
+++ src/main/java/mega/portal/paridhiportal/ParidhiPortalApplication.java
@@ -0,0 +1,13 @@
+package mega.portal.paridhiportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParidhiPortalApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ParidhiPortalApplication.class, args);
+  }
+
+}
---
+++ src/test/java/mega/portal/paridhiportal/ParidhiPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package mega.portal.paridhiportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParidhiPortalApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
